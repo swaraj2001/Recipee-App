5e0ee8c68eba0601b9228ac9b29e4d6c11cadefb
swaraj2001 <chimurkarswaraj@gmail.com>
2024-02-03 11:27:14 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://github.com/swaraj2001/RecipeeApp",
+  // base: "RecipeeApp",
   plugins: [react()],
 })
